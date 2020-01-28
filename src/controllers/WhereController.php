c099dd46ca3eb7989202daafb8ebcfba821cbde7
azaelsanchez <cem20619@gmail.com>
2020-01-28 17:38:14 +0100

    cambios varios

---
+++ src/controllers/WhereController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\controllers;
+
+
+use App\ViewManager;
+
+class WhereController extends Controller
+{
+    public function index()
+    {
+
+        $this->viewManager->renderTemplate("where.twig.html");
+    }
+}
